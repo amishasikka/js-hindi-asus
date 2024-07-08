41f6d974539cd4c522051f1cc33c088549ea1ddf
Amisha <141943974+amishasikka@users.noreply.github.com>
2024-07-08 13:24:56 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  //treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs. note browser
+
+console.log(3
+    +
+    3) //code readablity should be high
+
+console.log("Amisha")
+
+
+let name = "Amisha"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// DataTypes
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undeined => 
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
