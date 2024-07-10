2dcc64946a3082e82e3af39dd0034458e8a029dd
Amisha <141943974+amishasikka@users.noreply.github.com>
2024-07-10 14:21:22 +0000

    string functions

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,4 +5,23 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('amishaas')
+const gameName = new String('amisha-as')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(3,8);
+console.log(newString);
+
+
+
+
+
+
+
